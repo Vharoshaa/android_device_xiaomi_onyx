1fc3233e678a76307702bb4f4167d00eaf052b1a
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2026-03-15 18:30:42 +0100

    onyx: Enable CameraX extensions support
    - Set `ro.camerax.extensions.enabled=true` in both system.prop and odm.prop
    - Add `camerax-vendor-extensions.jar` to system_ext
    - Grant required permission via `camerax-vendor-extensions.xml`
    
    Enables support for enhanced camera features via CameraX Extensions, improving compatibility with modern camera apps.
    
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- device.mk
+++ device.mk
@@ -127,6 +127,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# CameraX Extensions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
